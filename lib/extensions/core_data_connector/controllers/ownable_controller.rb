251e096b671da8aa22814f660c737d2a6a4dd11a
Derek Leadbetter <dleadbetter55@gmail.com>
2023-08-31 16:09:00 -0400

    CDCN #2 - Updating ownable_controller to scope places by project

--- lib/extensions/core_data_connector/controllers/ownable_controller.rb
+++ lib/extensions/core_data_connector/controllers/ownable_controller.rb
@@ -6,6 +6,18 @@ module Extensions
       included do
         # Preloads
         preloads :project_item, :project
+
+        def base_query
+          return super unless params[:project_id]
+
+          item_class.where(
+            ProjectItem
+              .where(ProjectItem.arel_table[:projectable_id].eq(item_class.arel_table[:id]))
+              .where(projectable_type: item_class.to_s)
+              .where(project_id: params[:project_id])
+              .arel.exists
+          )
+        end
       end
     end
   end
