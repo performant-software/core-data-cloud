7f8e0e2fac507b4de83d15c734289e9fbfa8ebd9
Camden Mecklem <c@camdenmecklem.com>
2023-12-07 16:20:14 -0500

    add UUIDs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_024358) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_07_211713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -171,6 +171,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_29_024358) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }, null: false
     t.index ["project_model_id"], name: "index_core_data_connector_taxonomies_on_project_model_id"
   end
 
