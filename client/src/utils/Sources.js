d1ce4eb3bee515b7585caee07b5a17228d3eaf26
Camden Mecklem <c@camdenmecklem.com>
2023-12-20 12:40:52 -0500

    add name display logic for sources

--- client/src/utils/Sources.js
+++ client/src/utils/Sources.js
@@ -1,5 +1,29 @@
 // @flow
 import type { SourceTitle as SourceTitleType } from '../types/SourceTitle';
+import type { Source as SourceType } from '../types/Source';
+
+/**
+ * Returns the name to display for the passed source.
+ *
+ * @param source
+ *
+ * @returns {*}
+ */
+const getNameView = (source: SourceType) => {
+  if (source) {
+    const primary = source.primary_name;
+
+    if (primary) {
+      return source.primary_name.name.name;
+    }
+
+    if (source.source_titles && source.source_titles.length > 0) {
+      return source.source_titles[0].name.name;
+    }
+  }
+
+  return '';
+};
 
 const insertSourceTitle = (props: any, sourceTitle: SourceTitleType) => (
   props.onSetState({
@@ -11,5 +35,6 @@ const insertSourceTitle = (props: any, sourceTitle: SourceTitleType) => (
 );
 
 export default {
+  getNameView,
   insertSourceTitle
 };
