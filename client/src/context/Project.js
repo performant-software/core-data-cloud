1cde2359d3e4f56e997bd793049f4ce720c413b0
dleadbetter <>
2024-06-10 12:09:23 -0400

    CDC #194 - Updating the ProjectEdit component to reload the list of project models when switching from the "Settings" view to "Edit" view

--- client/src/context/Project.js
+++ client/src/context/Project.js
@@ -5,15 +5,19 @@ import type { Project as ProjectType } from '../types/Project';
 import type { ProjectModel as ProjectModelType } from '../types/ProjectModel';
 
 type ProjectContextType = {
+  loadedProjectModels: boolean,
   project: ProjectType,
   projectModel: ProjectModelType,
-  projectModels: Array<ProjectModelType>
+  projectModels: Array<ProjectModelType>,
+  reloadProjectModels: boolean
 };
 
 const ProjectContext = createContext<ProjectContextType>({
+  loadedProjectModels: false,
   project: undefined,
   projectModel: undefined,
-  projectModels: undefined
+  projectModels: undefined,
+  reloadProjectModels: false
 });
 
 export default ProjectContext;
