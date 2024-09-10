cb9b8f763b12504c2cd019fa84401db41196482e
dleadbetter <derek@performantsoftware.com>
2024-09-10 13:22:17 -0400

    CDC #287 - Fixing a bug in source_names migration

--- db/migrate/20240910171711_migrate_source_titles_to_source_names.core_data_connector.rb
+++ db/migrate/20240910171711_migrate_source_titles_to_source_names.core_data_connector.rb
@@ -2,13 +2,13 @@
 class MigrateSourceTitlesToSourceNames < ActiveRecord::Migration[7.0]
   def change
     execute <<-SQL.squish
-      INSERT INTO core_data_connector_source_names ( nameable_id, nameable_type, name, created_at, updated_at )
-      SELECT source_titles.name_id, source_titles.nameable_type, names.name, source_titles.created_at, source_titles.updated_at
+      INSERT INTO core_data_connector_source_names ( nameable_id, nameable_type, name, "primary", created_at, updated_at )
+      SELECT source_titles.nameable_id, source_titles.nameable_type, names.name, source_titles.primary, source_titles.created_at, source_titles.updated_at
         FROM core_data_connector_source_titles source_titles
         JOIN core_data_connector_names names ON names.id = source_titles.name_id
     SQL
 
-    execute <<-SQL.suqish
+    execute <<-SQL.squish
       DELETE
         FROM core_data_connector_source_names
        WHERE nameable_type = 'CoreDataConnector::Instance'
@@ -16,7 +16,7 @@ class MigrateSourceTitlesToSourceNames < ActiveRecord::Migration[7.0]
                                     FROM core_data_connector_instances )
     SQL
 
-    execute <<-SQL.suqish
+    execute <<-SQL.squish
       DELETE
         FROM core_data_connector_source_names
        WHERE nameable_type = 'CoreDataConnector::Item'
@@ -24,7 +24,7 @@ class MigrateSourceTitlesToSourceNames < ActiveRecord::Migration[7.0]
                                     FROM core_data_connector_items )
     SQL
 
-    execute <<-SQL.suqish
+    execute <<-SQL.squish
       DELETE
         FROM core_data_connector_source_names
        WHERE nameable_type = 'CoreDataConnector::Work'
