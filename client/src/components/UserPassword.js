79c4c69971c51d9897241d0ff7eeb8ad968410bd
Camden Mecklem <c@camdenmecklem.com>
2025-01-23 10:52:32 -0500

    hide password field for SSO users

--- client/src/components/UserPassword.js
+++ client/src/components/UserPassword.js
@@ -1,43 +1,61 @@
 // @flow
 
 import type { EditContainerProps } from '@performant-software/shared-components/types';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Form, Message } from 'semantic-ui-react';
 
 type Props = EditContainerProps;
 
+const SSO_DOMAINS = process.env.REACT_APP_SSO_DOMAINS
+  ? process.env.REACT_APP_SSO_DOMAINS.split(',')
+  : [];
+
 const UserPassword = (props: Props) => {
   const { t } = useTranslation();
 
-  if (props.item.sso_id || props.item.user?.sso_id) {
-    return <p>{t('UserPassword.messages.sso.cantChange')}</p>;
+  const showPasswordFields = useMemo(() => {
+    if (props.item.sso_id || props.item.user?.sso_id) {
+      return false;
+    }
+
+    const email = props.item.email || props.item.user?.email;
+
+    if (email) {
+      return !SSO_DOMAINS.some((domain) => email.endsWith(domain));
+    }
+
+    return true;
+  }, [props.item]);
+
+  if (showPasswordFields) {
+    return (
+      <>
+        <Message
+          content={t('UserPassword.messages.password.content')}
+          header={t('UserPassword.messages.password.header')}
+        />
+        <Form.Input
+          error={props.isError('password')}
+          label={t('UserPassword.labels.password')}
+          onChange={props.onTextInputChange.bind(this, 'password')}
+          required={props.isRequired('password')}
+          type='password'
+          value={props.item.password || ''}
+        />
+        <Form.Input
+          error={props.isError('password_confirmation')}
+          label={t('UserPassword.labels.passwordConfirmation')}
+          onChange={props.onTextInputChange.bind(this, 'password_confirmation')}
+          required={props.isRequired('password_confirmation')}
+          type='password'
+          value={props.item.password_confirmation || ''}
+        />
+      </>
+    );
   }
 
-  return (
-    <>
-      <Message
-        content={t('UserPassword.messages.password.content')}
-        header={t('UserPassword.messages.password.header')}
-      />
-      <Form.Input
-        error={props.isError('password')}
-        label={t('UserPassword.labels.password')}
-        onChange={props.onTextInputChange.bind(this, 'password')}
-        required={props.isRequired('password')}
-        type='password'
-        value={props.item.password || ''}
-      />
-      <Form.Input
-        error={props.isError('password_confirmation')}
-        label={t('UserPassword.labels.passwordConfirmation')}
-        onChange={props.onTextInputChange.bind(this, 'password_confirmation')}
-        required={props.isRequired('password_confirmation')}
-        type='password'
-        value={props.item.password_confirmation || ''}
-      />
-    </>
-  );
+  return <p>{t('UserPassword.messages.sso.cantChange')}</p>;
 };
 
 export default UserPassword;
