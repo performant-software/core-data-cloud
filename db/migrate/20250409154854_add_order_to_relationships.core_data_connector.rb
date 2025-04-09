eac621d57dd65549f54915c7997dc05c8cd3357c
Camden Mecklem <c@camdenmecklem.com>
2025-04-09 12:00:37 -0400

    update migration

--- db/migrate/20250409154854_add_order_to_relationships.core_data_connector.rb
+++ db/migrate/20250409154854_add_order_to_relationships.core_data_connector.rb
@@ -1,6 +1,6 @@
 # This migration comes from core_data_connector (originally 20250407150531)
 class AddOrderToRelationships < ActiveRecord::Migration[7.0]
   def change
-    add_column :core_data_connector_relationships, :order, :integer, default: 0
+    add_column :core_data_connector_relationships, :order, :integer
   end
 end
