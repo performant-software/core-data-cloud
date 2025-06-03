29618ca822c2693bd28c7ffcd553f65d83593fa9
dleadbetter <derek@performantsoftware.com>
2025-06-03 07:20:20 -0400

    CDC #440 - Installing migrations from "core_data_connector" gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_06_02_115230) do
+ActiveRecord::Schema[7.0].define(version: 2025_06_03_111812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -294,7 +294,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_02_115230) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
     t.uuid "sso_id"
     t.string "role"
   end
