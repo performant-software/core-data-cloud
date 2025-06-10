6dc40f58d66b4529eeec9288e57cceb2a0c340d4
dleadbetter <derek@performantsoftware.com>
2025-06-10 12:00:53 -0400

    CDC #441 - Adding user.role column to UserProjects page (for admin users only)

--- client/src/pages/UserProjects.js
+++ client/src/pages/UserProjects.js
@@ -15,6 +15,7 @@ import ProjectSettingsMenu from '../components/ProjectSettingsMenu';
 import UserEditMenu from '../components/UserEditMenu';
 import UserProjectRoles from '../utils/UserProjectRoles';
 import UserProjectsService from '../services/UserProjects';
+import UserRoles from '../utils/UserRoles';
 import UsersService from '../services/Users';
 import useParams from '../hooks/ParsedParams';
 import Validation from '../utils/Validation';
@@ -27,6 +28,51 @@ const UserProjects: AbstractComponent<any> = () => {
   const { t } = useTranslation();
 
   const ids = useMemo(() => ({ project_id: projectId, user_id: userId }), [projectId, userId]);
+
+  /**
+   * Memo-izes the columns to display in the list.
+   *
+   * @type {[]}
+   */
+  const columns = useMemo(() => {
+    const value = [];
+
+    if (!projectId) {
+      value.push({
+        name: 'core_data_connector_projects.name',
+        label: t('UserProjects.columns.project'),
+        resolve: (userProject) => userProject?.project.name,
+        sortable: true
+      });
+    }
+
+    if (!userId) {
+      value.push({
+        name: 'core_data_connector_users.name',
+        label: t('UserProjects.columns.user'),
+        resolve: (userProject) => userProject?.user.name,
+        sortable: true
+      });
+    }
+
+    value.push({
+      name: 'core_data_connector_user_projects.role',
+      label: t('UserProjects.columns.role'),
+      resolve: (userProject) => UserProjectRoles.getRoleView(userProject.role),
+      sortable: true
+    });
+
+    if (projectId && PermissionsService.canEditUsers()) {
+      value.push({
+        name: 'core_data_connector_users.role',
+        label: t('UserProjects.columns.type'),
+        resolve: (userProject) => UserRoles.getRoleView(userProject?.user?.role),
+        sortable: true
+      });
+    }
+
+    return value;
+  }, [projectId, userId]);
 
   /**
    * Fetch the current yser so we can display the name in the ItemHeader component.
@@ -99,24 +145,7 @@ const UserProjects: AbstractComponent<any> = () => {
           location: 'top',
           onClick: () => navigate('new')
         }}
-        columns={[{
-          name: 'core_data_connector_projects.name',
-          label: t('UserProjects.columns.project'),
-          resolve: (userProject) => userProject?.project.name,
-          sortable: true,
-          hidden: !!projectId
-        }, {
-          name: 'core_data_connector_users.name',
-          label: t('UserProjects.columns.user'),
-          resolve: (userProject) => userProject?.user.name,
-          sortable: true,
-          hidden: !!userId
-        }, {
-          name: 'core_data_connector_user_projects.role',
-          label: t('UserProjects.columns.role'),
-          resolve: (userProject) => UserProjectRoles.getRoleView(userProject.role),
-          sortable: true
-        }]}
+        columns={columns}
         collectionName='user_projects'
         defaultSort={projectId
           ? 'core_data_connector_users.name'
