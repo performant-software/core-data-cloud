623bec64a2d98bc79d0c7fcb19d37f7196d9e92b
dleadbetter <derek@performantsoftware.com>
2025-07-15 15:59:30 -0400

    CDC #458 - Adding comment to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disable Rails lazy route loading. Without this, the first request returns a 404 in development environments
 Rails.application.try(:reload_routes_unless_loaded) unless Rails.env.production?
