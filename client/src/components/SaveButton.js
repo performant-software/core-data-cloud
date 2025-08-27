6ab5af4bdf7b5934858e32f563fd60ec33d5ea1d
dleadbetter <derek@performantsoftware.com>
2025-08-27 11:51:23 -0400

    CDC #484 - Refactoring SaveButton to disable during saving state

--- client/src/components/SaveButton.js
+++ client/src/components/SaveButton.js
@@ -2,23 +2,33 @@
 
 import cx from 'classnames';
 import React from 'react';
-import { Container } from 'semantic-ui-react';
-import { SaveButton as SemanticSaveButton } from '@performant-software/semantic-components';
+import { useTranslation } from 'react-i18next';
+import { Button, Container } from 'semantic-ui-react';
 import styles from './SaveButton.module.css';
 
 type Props = {
-  onClick: () => void
+  onClick: () => void,
+  saving?: boolean
 };
 
-const SaveButton = (props: Props) => (
-  <Container
-    className={cx(styles.saveButton, styles.ui, styles.container)}
-    fluid
-  >
-    <SemanticSaveButton
-      onClick={props.onClick}
-    />
-  </Container>
-);
+const SaveButton = (props: Props) => {
+  const { t } = useTranslation();
+
+  return (
+    <Container
+      className={cx(styles.saveButton, styles.ui, styles.container)}
+      fluid
+    >
+      <Button
+        content={t('Common.buttons.save')}
+        disabled={props.saving}
+        primary
+        onClick={props.onClick}
+        size='medium'
+        type='submit'
+      />
+    </Container>
+  );
+};
 
 export default SaveButton;
