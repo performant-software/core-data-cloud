eb6b2df59cd5150811022bfebfc3302e732b24a4
Camden Mecklem <c@camdenmecklem.com>
2026-03-16 11:44:41 -0400

    roll back sso_invitation_id column

--- db/migrate/20260312133611_add_sso_invitation_id_to_users.core_data_connector.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from core_data_connector (originally 20260311205249)
-class AddSsoInvitationIdToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :core_data_connector_users, :sso_invitation_id, :string
-  end
-end
