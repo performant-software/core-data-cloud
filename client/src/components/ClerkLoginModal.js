23b3360c74cafd997558b58551cb238d9472c268
Camden <c@camdenmecklem.com>
2026-03-18 14:59:11 -0400

    Switch to `buildSignInUrl` for Clerk redirect (#578)
    * switch to buildSignInUrl
    
    * cleanup

--- client/src/components/ClerkLoginModal.js
+++ client/src/components/ClerkLoginModal.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
-import { SignInButton } from '@clerk/react';
+import { useClerk } from '@clerk/react';
 import { useTranslation } from 'react-i18next';
 import styles from './ClerkLoginModal.module.css';
 
 const ClerkLoginModal = () => {
   const { t } = useTranslation();
+
+  const { buildSignInUrl } = useClerk();
 
   return (
     <div
@@ -14,13 +16,13 @@ const ClerkLoginModal = () => {
       <h1 className={styles.clerkModalHeader}>
         {t('ClerkLoginModal.header')}
       </h1>
-      <SignInButton>
-        <Button
-          color='blue'
-        >
-          {t('ClerkLoginModal.login')}
-        </Button>
-      </SignInButton>
+      <Button
+        as='a'
+        color='blue'
+        href={buildSignInUrl()}
+      >
+        {t('ClerkLoginModal.login')}
+      </Button>
     </div>
   );
 }
