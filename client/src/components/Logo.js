121773ec8e0ea918b27ff2265250f1aabd2cb998
Jamie Folsom <jamiefolsom@gmail.com>
2026-05-26 10:57:41 -0400

    Rebrand UI from Core Data Cloud to FairData
    Updates page title, meta tags, PWA manifest, i18n strings, and the
    header Logo component. Replaces favicon and PWA icons with the new
    orange (#F15F26) FairData mark from Figma.
    
    Internal names (package.json, docker-compose, Heroku app slugs) and
    the app.coredata.cloud domain are intentionally untouched.

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,11 +1,8 @@
 // @flow
 
-import cx from 'classnames';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { TbDatabaseShare } from 'react-icons/tb';
 import { Link } from 'react-router';
-import { Header } from 'semantic-ui-react';
 import styles from './Logo.module.css';
 
 const Logo = () => {
@@ -16,12 +13,10 @@ const Logo = () => {
       className={styles.logo}
       to='/projects'
     >
-      <TbDatabaseShare
-        size='2em'
-      />
-      <Header
-        className={cx(styles.ui, styles.header)}
-        content={t('Logo.title')}
+      <img
+        alt={t('Logo.title')}
+        className={styles.image}
+        src='/logo.svg'
       />
     </Link>
   );
